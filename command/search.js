da0fcbcee7e85fa6a9bb547f4892013dacf07adc
James Tracy <jtracy.emmet@gmail.com>
2020-02-26 19:31:18 -0500

    better parse + actually works!

--- command/search.js
+++ command/search.js
@@ -1,6 +1,9 @@
 
+require('dotenv').config();
+const request = require('request');
 const configs = require('../configs.js');
 const { listEmoji } = require('../helpers.js');
+const { RichEmbed } = require('discord.js');
 
 module.exports = {
   meta: {
@@ -20,7 +23,16 @@ module.exports = {
     },
   },
 
-  execute: function (msg, args) {
-    msg.channel.send(`Results for ${args.keyword}`);
+  execute: async function (msg, args) {
+    /*
+    request.get(`${configs.searchURL}&key=${process.env.IMG_API_KEY}&q=${args.keyword}&searchType=image&num=1`,
+    (err, res, body) => {
+      if(err) msg.channel.send('Something went wrong.');
+      const { items } = JSON.parse(body);
+      console.log(items);
+      if(items.length === 0) return msg.channel.send('No results!');
+      msg.channel.send(items[0].link);
+    });
+    */
   }
 };
